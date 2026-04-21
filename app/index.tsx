19aa466e540bc460bba1344f5e01acf25c86f459
verzyo <244832183+verzyo@users.noreply.github.com>
2026-04-21 20:17:14 +0000

    fix: url regex

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { Redirect } from "expo-router";
 import { Platform } from "react-native";
 
-
 export default function LandingScreen() {
 	if (Platform.OS !== "web") {
 		return null;
--- components/AttachmentBottomSheet.web.tsx
+++ components/AttachmentBottomSheet.web.tsx
@@ -115,7 +115,8 @@ const AttachmentBottomSheet = forwardRef<AttachmentBottomSheetRef, Props>(
 				return;
 			}
 			if (url.trim()) {
-				const urlRegex = /^https?:\/\/.+/;
+				const urlRegex =
+					/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#?&//=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%._+~#?&//=]*)$/i;
 				if (!urlRegex.test(url.trim())) {
 					setUrlError("Provide valid URL");
 					return;
